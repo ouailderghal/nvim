60c080cbc48c25c6bd6b31379e0f7be7f04dc5f4
Ouail Derghal <ouail.derghal@imt-atlantique.fr>
2024-02-08 16:31:46 +0100

    Add keymaps.lua

---
+++ lua/ouailderghal/keymaps.lua
@@ -0,0 +1,23 @@
+-- [[ Keymaps ]]
+
+-- Keymaps for better default experience
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+-- Custom keymap
+vim.keymap.set('n', '<leader>bd', '<cmd>:bdelete<cr>')
+vim.keymap.set('n', '<leader>qq', '<cmd>:quit!<cr>')
+vim.keymap.set('n', '<leader>op', '<cmd>:Neotree toggle<cr>')
+vim.keymap.set('n', '<C-n>', '<cmd>:bn<cr>')
+vim.keymap.set('n', '<C-p>', '<cmd>:bp<cr>')
+vim.keymap.set('n', '<leader>zz', '<cmd>:ZenMode<cr>', { desc = 'Toggle Zen mode' })
+vim.keymap.set('n', '<leader>ff', '<cmd>:Format<cr>', { desc = '[F]ormat [F]ile'})
