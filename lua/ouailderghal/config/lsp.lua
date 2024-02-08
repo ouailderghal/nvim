6383bf8dc30df3d00af13bcb8042e73e0615523b
Ouail Derghal <ouail.derghal@imt-atlantique.fr>
2024-02-08 16:48:22 +0100

    Add pyright to config/lsp.lua

--- lua/ouailderghal/config/lsp.lua
+++ lua/ouailderghal/config/lsp.lua
@@ -78,6 +78,7 @@ require('mason-lspconfig').setup()
 --  define the property 'filetypes' to the map in question.
 local servers = {
   clangd = {},
+  pyright = {},
   gopls = {},
   tsserver = {},
   solargraph=  {},
