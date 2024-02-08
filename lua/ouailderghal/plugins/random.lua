6f19debfab0f0fff4346d9d00cea8daa292c0f1f
Ouail Derghal <ouail.derghal@imt-atlantique.fr>
2024-02-08 16:29:53 +0100

    Add plugins/random.lua

---
+++ lua/ouailderghal/plugins/random.lua
@@ -0,0 +1,10 @@
+return {
+  'tpope/vim-fugitive',
+  'tpope/vim-rhubarb',
+  'tpope/vim-sleuth',
+  'vimwiki/vimwiki',
+  'folke/zen-mode.nvim',
+  'apple/pkl-neovim',
+
+  { "numToStr/Comment.nvim", opts = {} },
+}
